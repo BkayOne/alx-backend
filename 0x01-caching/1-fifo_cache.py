f464c9242b72dbc3fe336571f2eab7c7ad1ae552
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-10-25 23:16:46 +0000

    FIFO fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python3
-"""defines a basic fifo cache system."""
-from collections import OrderedDict
-from base_caching import BaseCaching
+
+"""Basic dictionary"""
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """
-    defines a simple fifo caching system.
-    """
-    def __inti__(self):
-        """initialize cache storage"""
-        super().__init()
-        self.cache_data = OrderedDict()
+    """FIFO Cache Class"""
+    def __init__(self):
+        """Iniitialization"""
+        super().__init__()
 
     def put(self, key, item):
-        """implements a put system.
-        """
-        if key is not None and item is not None:
+        """Puts data into cache using the FIFO system"""
+        if (key is None or item is None):
+            pass
+        else:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                del_key = next(iter(self.cache_data))
+                if self.cache_data.get(key) is None:
+                    del self.cache_data[del_key]
+                    print("DISCARD: {}".format(del_key))
             self.cache_data[key] = item
-            if len(self.cache_data) > super().MAX_ITEMS:
-                d_item = list(self.cache_data.keys())[0]
-                del (self.cache_data[d_item])
-                print(f'Discard: {d_item}')
 
     def get(self, key):
-        """
-        returns the value linked to the key.
-        returns None if the key or the value doesn't exist.
-        """
-        return self.cache_data.get(key, None)
+        """Gets from the cache"""
+        return self.cache_data.get(key)
